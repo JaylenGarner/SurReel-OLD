329f04e856736cb053f99f74ac45dbf011786d18
Jaylen Garner <jaylen.garner@outlook.com>
2023-01-22 18:52:26 -0500

    Feed styling checkpoint

--- react-app/src/components/HomeFeed/HomeFeed.js
+++ react-app/src/components/HomeFeed/HomeFeed.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadFeedPostsThunk } from '../../store/posts';
+import './HomeFeed.css'
 
 
 function HomeFeed() {
@@ -22,13 +23,21 @@ function HomeFeed() {
     return <h1>There are no posts in your feed. Get started by following other users</h1>
   } else {
       return (
-       <div>
+       <div className='home-page-feed-container'>
         {posts && Object.values(posts).map((post) => {
-            return <div key={post.id}>
-                <img src={post.media}></img>
+            return <div key={post.id} className='home-feed-post-container'>
+                <div className='home-feed-post-owner-info-container'>
+                <img src={post.owner.image} className='home-feed-post-owner-image'></img>
+                <span className='home-feed-post-owner-username'>{post.owner.username}</span>
+                </div>
+                <img src={post.media} className='home-feed-post-image'></img>
+                <div className='home-feed-post-interaction-area'>
+
                 <span>{post.caption}</span>
+                </div>
             </div>
         })}
+        <div className='home-page-info-area'>User info area / suggested users</div>
        </div>
       );
   }
