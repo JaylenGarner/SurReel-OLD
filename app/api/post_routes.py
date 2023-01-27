fc95fe10b6b072b2b1de4262e7f3406744482f71
Jaylen Garner <jaylen.garner@outlook.com>
2023-01-27 14:40:01 -0500

    Like API routes complete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import db, User, Post
+from sqlalchemy import and_
+from app.models import db, User, Post, Like
+
 
 post_routes = Blueprint('posts', __name__)
 
@@ -41,3 +43,45 @@ def delete_post(id):
     db.session.commit()
 
     return 'The post has been deleted'
+
+
+@post_routes.route('/<int:id>/likes')
+def get_post_likes(id):
+
+    res = {}
+
+    likes = Like.query.filter(Like.post_id == id).all()
+
+    for like in likes:
+        el = like.to_dict()
+        res[f'{el["id"]}'] = el
+
+    return res
+
+@post_routes.route('/<int:id>/like')
+def like_post(id):
+
+    post = Post.query.get(id)
+
+    like = Like (
+        user_id = current_user.id,
+        post_id = id
+    )
+
+    db.session.add(like)
+    db.session.commit()
+
+    return post.to_dict()
+
+
+@post_routes.route('/<int:id>/unlike')
+def unlike_post(id):
+
+    like = Like.query.filter(and_(Like.user_id == current_user.id,
+    Like.post_id == id)).first()
+
+
+    db.session.delete(like)
+    db.session.commit()
+
+    return 'You have unliked the post'
