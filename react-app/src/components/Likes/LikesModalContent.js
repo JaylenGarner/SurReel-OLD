6b4d7dbf85b5c91c2ec17182bcefbdf0ec11d2fb
Jaylen Garner <jaylen.garner@outlook.com>
2023-01-29 11:49:36 -0500

    Read complete for likes

--- react-app/src/components/Likes/LikesModalContent.js
+++ react-app/src/components/Likes/LikesModalContent.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadLikesThunk } from '../../store/likes';
 import '../profile/FollowModalContent/FollowersModalContent.css'
 
-const LikesModalContent = ({setModalIsOpen, postId}) => {
+const LikesModalContent = ({setModalIsOpen, postId, postPage}) => {
     const dispatch = useDispatch()
     const posts = useSelector((state) => state.posts)
     let post = posts[postId]
@@ -12,6 +12,25 @@ const LikesModalContent = ({setModalIsOpen, postId}) => {
     useEffect(() => {
         // dispatch(loadLikesThunk(postId));
       }, [dispatch]);
+
+      if (postPage) {
+        return (
+          <div>
+                    {Object.values(posts.post.likes).map((like) => {
+                        return (
+                        <div className='followers-modal-user-container' key={like.id}>
+                            <NavLink  className='followers-modal-nav-link' to={`/users/${like.user.id}/profile`} onClick={() => setModalIsOpen(false)}>
+                              <img className='followers-modal-user-image' src={like.user.image}></img>
+                            </NavLink>
+                            <NavLink  className='followers-modal-nav-link' to={`/users/${like.user.id}/profile`} onClick={() => setModalIsOpen(false)}>
+                              <span className='followers-modal-username'>{like.user.username}</span>
+                            </NavLink>
+                        </div>
+                        )
+                    })}
+                </div>
+        )
+      }
 
             return (
                 <div>
--- react-app/src/components/PostPage/PostPage.js
+++ react-app/src/components/PostPage/PostPage.js
@@ -5,6 +5,7 @@ import { loadPostThunk } from '../../store/posts';
 import { deletePostThunk } from '../../store/posts';
 import Modal from 'react-modal'
 import EditPostForm from './EditPostForm';
+import LikesModalContent from '../Likes/LikesModalContent';
 import './PostPage.css';
 
 function PostPage() {
@@ -15,6 +16,8 @@ function PostPage() {
   const { postId } = useParams()
 
   const [editCaptionModalIsOpen, setEditCaptionModalIsOpen] = useState(false);
+  const [likesModalIsOpen, setLikesModalIsOpen] = useState(false);
+
 
   const handleDelete = (postId) => {
     dispatch(deletePostThunk(postId))
@@ -38,14 +41,17 @@ function PostPage() {
             <img src={post.owner.image} className='post-page-user-image'></img>
           </NavLink>
           <NavLink to={`/users/${post.owner.id}/profile`} className='post-page-owner-username'>{post.owner.username}</NavLink>
+          {user.id == post.owner.id && <button onClick={() => setEditCaptionModalIsOpen(true)} className='edit-caption-button'>Edit caption</button>}
+          {user.id == post.owner.id &&<button onClick={() => handleDelete(postId)} className='delete-post-button'>Delete Post</button>}
         </div>
         <div className='post-page-caption-area'>
           <img src={post.owner.image} className='post-page-user-image'></img>
           <NavLink to={`/users/${post.owner.id}/profile`} className='post-page-owner-username'>{post.owner.username}</NavLink>
           <span className='post-page-caption'>{post.caption}</span>
+          <div className='post-page-interaction-area-container'>
+              <span onClick={() => setLikesModalIsOpen(true)} className='post-page-likes-modal-button'>Likes</span>
+             </div>
           <div>
-            {user.id == post.owner.id && <button onClick={() => setEditCaptionModalIsOpen(true)}>Edit caption</button>}
-            {user.id == post.owner.id &&<button onClick={() => handleDelete(postId)}>Delete Post</button>}
             <div className='edit-post-caption-modal'>
               <Modal
               isOpen={editCaptionModalIsOpen}
@@ -63,6 +69,25 @@ function PostPage() {
               >
                 <EditPostForm currentCaption={post.caption} setEditCaptionModalIsOpen={setEditCaptionModalIsOpen}/>
                 <button onClick={() => setEditCaptionModalIsOpen(false)}>Cancel</button>
+             </Modal>
+             <Modal
+              isOpen={likesModalIsOpen}
+              style={{
+                content: {
+                  width: '400px',
+                  height: '400px',
+                  top: '18%',
+                  left: '42%',
+                  backgroundColor: '#262626',
+                  color: 'white',
+                }
+              }}
+              >
+                <div className='profile-following-modal-header-container'>
+                  <span className='profile-following-modal-header-text'>Likes</span>
+                </div>
+                <button onClick={() => setLikesModalIsOpen(false)} className='profile-following-modal-close-button'>X</button>
+                <LikesModalContent setModalIsOpen={setLikesModalIsOpen} postId={post.id} postPage={true}/>
              </Modal>
             </div>
           </div>
