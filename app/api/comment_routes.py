870871c48e094c526fa75913ff590a5134aae1ad
Jaylen Garner <jaylen.garner@outlook.com>
2023-02-19 11:19:53 -0500

    Edit and delete comment API routes complete

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -5,37 +5,38 @@ from app.models import db, Comment
 comment_routes = Blueprint('comments', __name__)
 
 
-# @message_routes.route('/<int:id>/edit', methods=['PUT'])
-# @login_required
-# def edit_message(id):
+@comment_routes.route('/<int:id>/edit', methods=['PUT'])
+@login_required
+def edit_comment(id):
 
-#     message = Message.query.get(id)
+    comment = Comment.query.get(id)
 
-#     if message == None:
-#         return {"msg": "A message could not be found by the requested ID"}
+    if comment == None:
+        return {"msg": "A comment could not be found by the requested ID"}
 
-#     if message.user_id != current_user.id:
-#         return {"msg": "This is not your message"}
+    if comment.user_id != current_user.id:
+        return {"msg": "This is not your comment"}
 
-#     message.body = request.json["body"]
-#     db.session.commit()
+    comment.body = request.json["body"]
 
-#     return message.to_dict()
+    db.session.commit()
+
+    return comment.to_dict()
 
 
-# @message_routes.route('/<int:id>/delete', methods=['DELETE'])
-# @login_required
-# def delete_message(id):
+@comment_routes.route('/<int:id>/delete', methods=['DELETE'])
+@login_required
+def delete_message(id):
 
-#     message = Message.query.get(id)
+    comment = Comment.query.get(id)
 
-#     if message == None:
-#         return {"msg": "A message could not be found by the requested ID"}
+    if comment == None:
+        return {"msg": "A message could not be found by the requested ID"}
 
-#     if message.user_id != current_user.id:
-#         return {"msg": "This is not your message"}
+    if comment.user_id != current_user.id:
+        return {"msg": "This is not your message"}
 
-#     db.session.delete(message)
-#     db.session.commit()
+    db.session.delete(comment)
+    db.session.commit()
 
-#     return {"msg": 'Message has been deleted'}
+    return {"msg": 'Message has been deleted'}
